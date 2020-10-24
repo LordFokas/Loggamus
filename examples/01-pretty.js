12e022672ea0fd624b7ab7b3c1502aa974b6b4d6
LordFokas <lordfokas@gmail.com>
2020-10-24 21:33:16 +0100

    Fix small stuff, add pretty printing example

---
+++ examples/01-pretty.js
@@ -0,0 +1,54 @@
+console.log("\n\n\n");
+
+
+import { PrettyPrinter } from '../index.js';
+
+const pretty = new PrettyPrinter();
+
+// All printer methods return "this", so you can chain anything.
+pretty
+	.color('green') // set foreground color. There's .background() too.
+	.write('Sometimes, ') // write text
+	.style('underline', 'bright') // change style
+	.color('red')
+	.write('all', ' you', ' want') // takes multiple args
+	.endl() // add 1 '\n'. If you pass it a number, it adds that many.
+	.reset() // reset the styles -- is a shortcut to style('reset')
+	.style('bright') // could have been style('reset', 'bright') instead
+	.color('yellow') // unfortunately it also resets all the colors :(
+	.write('is some pretty text!');
+
+	// The printer stores everything in an internal buffer
+	// and won't write anything until you flush it.
+	// When you flush it, it prints the whole buffer as a single message.
+	// Flushing also resets the printer to the initial state
+	// and returns "this" so you can keep going with the same instance.
+pretty.flush();
+
+// All the colors. Dim and Bright variations. Default style is Dim.
+pretty.endl();
+for(const c of [
+	'black', 'red', 'green', 'yellow',
+	'blue', 'magenta', 'cyan', 'white'
+]){
+	let bg = 'black';
+	if(c == 'black') bg = 'white';
+	pretty.background(bg).color(c).write(' test ').style('reverse').write(' test ').flush();
+	pretty.style('bright');
+	pretty.background(bg).color(c).write(' test ').style('reverse').write(' test ').flush();
+}
+
+// All the styles
+pretty.endl();
+pretty.style('underline').write('Underline. Underscore is also an alias.').flush();
+pretty.style('bright').write('Bright makes foreground colors brighter. Duh.').flush();
+pretty.style('dim').write('Dim makes colors dimmer. This is the default console style.').flush();
+pretty.style('reverse').write('Reverse swaps background and foreground colors.').flush();
+pretty.endl();
+pretty.color('red').style('bright', 'underline')
+	.write('I have no idea how to make these work, but I mapped them from the VT100 spec anyways:').flush();
+pretty.style('blink').write('I have no idea how Blink works, but the control char is mapped. Good luck.').flush();
+pretty.style('hidden').write('Hidden is also weird, at least in my terminal. But here you go.').flush();
+
+
+console.log("\n\n\n");
--- examples/02-override.js
+++ examples/02-override.js
@@ -2,18 +2,17 @@ console.log("\n\n\n");
 
 
 import { Logger, LogLevel, Pipe, Output } from '../index.js';
-Logger.setAppRoot(3);
+Logger.setAppRoot(3); // paths in stack traces start 2 levels above this file
 
 Logger.setDefault(new Logger('root', {
-	minlevel: LogLevel.WARN,
-	mintrace: LogLevel.FATAL,
-	tracedepth: 5,
-	output: new Pipe(
-		new Output.Terminal(),
-		new Output.File('./examples.log')
+	minlevel: LogLevel.WARN, // Logs below WARN will not log
+	mintrace: LogLevel.FATAL, // Logs below FATAL won't display stack traces
+	tracedepth: 5, // stack traces are at most 5 stack frames deep
+	output: new Pipe( // log receptacle that replicates logs it receives between its children
+		new Output.Terminal(), // log receptacle that writes pretty logs to terminal
+		new Output.File('./examples.log') // log receptacle that writes raw logs to disk
 	)
 }));
-
 
 (function (){
 	Logger.fatal("This is a fatal error!", {
@@ -28,8 +27,11 @@ Logger.setDefault(new Logger('root', {
 	Logger.error("This is a recoverable error...");
 })();
 
+// Child logger will inherit everything from the parent, unless we override it.
 const child = Logger.getDefault().child('child-01');
 child.warn("I'll only warn you this once.");
+
+// these will never be seen because the log level is too high
 child.info("Information is the currency of modern times...");
 child.debug("Debugging: The fun mystery game where you are\nthe detective, the victim, and the murderer!!");
 child.fine("This level of information is so fine that it probably doesn't matter.");
