3ffe1d714f231a313eb77d284d5d1420ac3f3220
LordFokas <lordfokas@gmail.com>
2020-10-31 02:45:03 +0000

    Fix mappers, add filters and predicates

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ export { PrettyPrinter } from './lib/printer/PrettyPrinter.js';
 export { Output } from './lib/pipeline/Output.js';
 export { Pipe } from './lib/pipeline/Pipe.js';
 export { Mapper } from './lib/pipeline/Mapper.js';
+export { Filter } from './lib/pipeline/Filter.js';
+export { Predicates } from './lib/pipeline/Predicates.js';
